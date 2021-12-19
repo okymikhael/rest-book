a7b4dbd6eb5143773519b044100e86db6f0b4dcb
Oky Mikhael <okymikhael@gmail.com>
2021-12-19 21:51:55 +0700

    first commit

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Book;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $skip = request('skip') ?? 0;
+        $limit = request('limit') ?? 10;
+        $sort = request('sort') ?? 'desc';
+
+        $book = Book::skip($skip)->limit($limit)->limit($limit)->orderBy('id', $sort)->get();
+        return response()->json(['message' => 'success', 'data' => $book], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreBookRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBookRequest $request)
+    {
+        //
+        $book = Book::create($request->all());
+        return response()->json(['message' => 'success', 'data' => $request->all()], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        $book = Book::find($book);
+        return response()->json(['message' => 'success', 'data' => $book], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateBookRequest  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBookRequest $request, Book $book)
+    {
+        $book = $book->update($request->all());
+        return response()->json(['message' => 'success', 'data' => $request->all()], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        $book = $book->delete();
+        return response()->json(['message' => 'success', 'data' => $book], 201);
+    }
+}
